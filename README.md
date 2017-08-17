a449904c00cee8c65ba24194eedf8ca69e71a6df
Dave Kaufman <dave@davekaufman.net>
2017-08-16 21:29:51 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# mp-th
-exercise
+# mp exercise
+
+## Requirements
+You have a distributed system comprised of multiple server hosts (e.g. 10-15 AWS instances). The hosts run a common task and need to have a long-running (multi-hour) maintenance-type operation run against them. The requirements are:
+
+- The operation cannot run on more than one host at any given time
+- The operation cannot run more than one instance at a time on any host
+- It must log its activity for tracking purposes (to syslog and local file)
+- It must report 5 metrics about its work to a DogStatsD agent running on the host, as defined in DataDog’s documentation (https://docs.datadoghq.com/guides/dogstatsd/)
+- It must offer a way to abort the task gracefully (not killing the task via kill command)
+- It must be self-documenting, well commented, for clarity and flow for anyone not familiar with the original specs.
+
+## Prerequisites / Assumptions
+- datadog agent running on each instance
+- S3 bucket for lockfile checking
+- management instance/machine with ansible installed, ssh access to all instances, and access to s3 for lockfile tracking (easily and more properly moved to instances instead via instance role)
+
+## Usage
+Invocation is like any ansible playbook.
+```shell
+ansible-playbook -i hosts maintenance.yml -K
+```
---
+++ roles/maintenance/files/maintenance_script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Description: unspecified maintenance task
+
+# useful vars
+dd_port=8125                              # dogstatsd port
+dd_host=localhost                         # dogstatsd host
+facility="local0.notice"                  # syslog facility / level
+logfile="/var/log/maintenance_script.log" # logfile
+
+
+# have we met before?
+[[ -f "/tmp/maintenance-completed" ]] && echo "previously run successfully" && exit 0
+
+# log start to syslog and file via logger
+logger -p ${facility} -t maintenance_script "beginning run of maintenance script"  -s 2>${logfile}
+
+# log start as datadog event
+# viz. https://help.datadoghq.com/hc/en-us/articles/206441345-Send-metrics-and-events-using-dogstatsd-and-the-shell
+event_title="maintenance script start"
+event_text="$(hostname)"
+echo "_e{${#event_title},${#event_text}}:${event_title}|${event_text}|#maintenance_script"  >/dev/udp/${dd_host}/${dd_port}
+
+# first datadog metric 
+echo -n "custom_metric_1:$RANDOM|g|#maintenance_script" >/dev/udp/${dd_host}/${dd_port}	
+
+logger -p ${facility} -t maintenance_script "starting sleep in lieu of real maintenance tasks"  -s 2>${logfile}
+
+# sleep here is standing in for whatever the unspecified long-running maintenance tasks are
+sleep 2m || exit 1
+
+# custom metrics 2 and 3 
+echo -n "custom_metric_2:$RANDOM|g|#maintenance_script" >/dev/udp/${dd_host}/${dd_port}	
+echo -n "custom_metric_3:$RANDOM|g|#maintenance_script" >/dev/udp/${dd_host}/${dd_port}	
+
+# end
+logger -p ${facility} -t maintenance_script "ending successful run of maintenance script"  -s 2>${logfile}
+
+# log completion of script as datadog event
+event_title="maintenance script end"
+event_text="$(hostname)"
+echo "_e{${#event_title},${#event_text}}:${event_title}|${event_text}|#maintenance_script"  >/dev/udp/${dd_host}/${dd_port}
+
+# write sempahore for pseudo-idempotency
+touch /tmp/maintenance_completed
+
